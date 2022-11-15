66fe6107fc03c8eb50e496f1523f71dbc3170924
ananjaykumar2 <ananjaykumar2@gmail.com>
2022-11-15 18:01:33 +0530

    package info modify

--- src/main/java/iudx/auditing/server/immuDb/package-info.java
+++ src/main/java/iudx/auditing/server/immuDb/package-info.java
@@ -1,5 +1,5 @@
 @ModuleGen(groupPackage = "udx.auditing.server.immuDb",
-        name = "iudx-auditing-server-immuDb-service")
+        name = "iudx-auditing-server-immuDb-service",useFutures = true)
 package iudx.auditing.server.immuDb;
 
 import io.vertx.codegen.annotations.ModuleGen;
