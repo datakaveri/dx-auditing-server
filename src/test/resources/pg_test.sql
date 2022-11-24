7809b92c1309d1eabb88893c1d23132aae85c069
ankitmashu <ankitmashu@gmail.com>
2022-11-24 14:27:04 +0530

    Modified POM, Added testScript for postgres

---
+++ src/test/resources/pg_test.sql
@@ -0,0 +1,24 @@
+CREATE TABLE IF NOT EXISTS auditing_rs
+(
+   id varchar NOT NULL,
+   api varchar NOT NULL,
+   userid varchar NOT NULL,
+   epochtime numeric NOT NULL,
+   resourceid varchar NOT NULL,
+   isotime varchar NOT NULL,
+   providerid varchar NOT NULL,
+   size numeric NOT NULL
+);
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+   
