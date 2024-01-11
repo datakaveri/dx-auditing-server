ee4c81ac5585a0f8de347116dbd478a4ea070ee0
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2024-01-11 14:05:31 +0530

    Add test cases

--- src/test/java/iudx/auditing/server/processor/MessageProcessorImplTest.java
+++ src/test/java/iudx/auditing/server/processor/MessageProcessorImplTest.java
@@ -10,6 +10,8 @@ import iudx.auditing.server.postgres.PostgresService;
 import iudx.auditing.server.processor.subscription.SubscriptionAuditService;
 import iudx.auditing.server.processor.subscription.SubscriptionAuditServiceImpl;
 import iudx.auditing.server.rabbitmq.RabbitMqService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,17 +19,23 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
 import static iudx.auditing.server.common.Constants.*;
 import static iudx.auditing.server.querystrategy.util.Constants.*;
 import static javax.xml.transform.OutputKeys.METHOD;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 @ExtendWith({VertxExtension.class, MockitoExtension.class})
 class MessageProcessorImplTest {
+    private static final Logger LOGGER = LogManager.getLogger(MessageProcessorImplTest.class);
+
 
     @Mock
     PostgresService postgresService;
@@ -132,6 +140,103 @@ class MessageProcessorImplTest {
         assertTrue(resultJson.result().containsKey(PG_DELETE_QUERY_KEY));
         assertTrue(resultJson.result().containsKey(IMMUDB_WRITE_QUERY));
         vertxTestContext.completeNow();
+    }
+
+
+    @Test
+    @DisplayName("Test processAuditEventMessages method for DMP server audit logs: Success")
+    public void testForDmpSuccess(VertxTestContext vertxTestContext)
+    {
+        ZonedDateTime zst = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        long time = zst.toInstant().toEpochMilli();
+        String isoTime = zst.truncatedTo(ChronoUnit.SECONDS).toString();
+
+        String primaryKey = UUID.randomUUID().toString().replace("-", "");
+        String userId = UUID.randomUUID().toString();
+        String information = new JsonObject().put("policyId", UUID.randomUUID().toString()).put("createdAt", isoTime).put("policyStatus","ACTIVE").encode();
+        LOGGER.info("primary key is : {}", primaryKey);
+        when(config.getString(DMP_APD_PG_TABLE_NAME)).thenReturn("DMP_APD_PG_TABLE_NAME");
+        when(config.getString(DMP_APD_IMMUDB_TABLE_NAME)).thenReturn("DMP_APD_IMMUDB_TABLE_NAME");
+        when(immudbService.executeWriteQuery(any())).thenReturn(Future.succeededFuture());
+        when(postgresService.executeWriteQuery(any())).thenReturn(Future.succeededFuture());
+    message =
+        new JsonObject()
+            .put(ORIGIN, DMP_APD_SERVER)
+            .put(DELIVERY_TAG, Long.valueOf(12334245))
+            .put(PRIMARY_KEY, primaryKey)
+            .put(USER_ID, userId)
+            .put(INFORMATION, information)
+            .put(API, "/dx/apd/dmp/v1/policies")
+            .put(HTTP_METHOD, "POST")
+            .put(EPOCH_TIME, time)
+            .put(ISO_TIME, isoTime)
+            .put(DMP_APD_IMMUDB_TABLE_NAME, "DMP_APD_IMMUDB_TABLE_NAME")
+            .put(DMP_APD_PG_TABLE_NAME, "DMP_APD_PG_TABLE_NAME");
+
+    messageProcessor.processAuditEventMessages(message).onComplete(handler -> {
+        if(handler.succeeded())
+        {
+            LOGGER.info("Success : " + handler.result().encodePrettily());
+            JsonObject result = handler.result();
+            assertEquals(result.getLong(DELIVERY_TAG), Long.valueOf(12334245));
+            assertEquals(result.getString(ORIGIN),DMP_APD_SERVER);
+            assertTrue(result.containsKey(PG_INSERT_QUERY_KEY));
+            assertTrue(result.containsKey(PG_DELETE_QUERY_KEY));
+            assertTrue(result.containsKey(IMMUDB_WRITE_QUERY));
+            vertxTestContext.completeNow();
+
+        }
+        else
+        {
+            LOGGER.error("Failure");
+            vertxTestContext.failNow("Failure");
+        }
+    });
+    }
+
+    @Test
+    @DisplayName("Test processAuditEventMessages method for DMP server audit logs when executeWriteQuery for postgres fails : Failure")
+    public void testForDmpFailure(VertxTestContext vertxTestContext)
+    {
+        ZonedDateTime zst = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        long time = zst.toInstant().toEpochMilli();
+        String isoTime = zst.truncatedTo(ChronoUnit.SECONDS).toString();
+
+        String primaryKey = UUID.randomUUID().toString().replace("-", "");
+        String userId = UUID.randomUUID().toString();
+        String information = new JsonObject().put("policyId", UUID.randomUUID().toString()).put("createdAt", isoTime).put("policyStatus","ACTIVE").encode();
+        LOGGER.info("primary key is : {}", primaryKey);
+        when(config.getString(DMP_APD_PG_TABLE_NAME)).thenReturn("DMP_APD_PG_TABLE_NAME");
+        when(config.getString(DMP_APD_IMMUDB_TABLE_NAME)).thenReturn("DMP_APD_IMMUDB_TABLE_NAME");
+        when(postgresService.executeWriteQuery(any())).thenReturn(Future.failedFuture("Some failure"));
+        message =
+                new JsonObject()
+                        .put(ORIGIN, DMP_APD_SERVER)
+                        .put(DELIVERY_TAG, Long.valueOf(12334245))
+                        .put(PRIMARY_KEY, primaryKey)
+                        .put(USER_ID, userId)
+                        .put(INFORMATION, information)
+                        .put(API, "/dx/apd/dmp/v1/policies")
+                        .put(HTTP_METHOD, "POST")
+                        .put(EPOCH_TIME, time)
+                        .put(ISO_TIME, isoTime)
+                        .put(DMP_APD_IMMUDB_TABLE_NAME, "DMP_APD_IMMUDB_TABLE_NAME")
+                        .put(DMP_APD_PG_TABLE_NAME, "DMP_APD_PG_TABLE_NAME");
+
+        messageProcessor.processAuditEventMessages(message).onComplete(handler -> {
+            if(handler.succeeded())
+            {
+                LOGGER.info("Success : " + handler.result().encodePrettily());
+                vertxTestContext.failNow("Succeeded when the postgres write query failed");
+
+            }
+            else
+            {
+                LOGGER.error("Failed to execute postgres query");
+                assertTrue( handler.cause().getMessage().contains("failed to insert in postgres"));
+                vertxTestContext.completeNow();
+            }
+        });
     }
 
     @Test
