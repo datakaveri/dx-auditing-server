1c87e3cf3ce34e0ceea8ac0f62c2325a94363c08
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2024-04-25 11:32:02 +0530

    Update immudb schema

--- src/main/resources/immudb/migration/V1_1__init-tables.sql
+++ src/main/resources/immudb/migration/V1_1__init-tables.sql
@@ -6,31 +6,35 @@
 ---
 --- auditing_dmp
 ---
-exec CREATE TABLE auditing_dmp(_id VARCHAR[128] NOT NULL,user_id VARCHAR[128] NOT NULL,api VARCHAR[128] NOT NULL,method VARCHAR[32] NOT NULL,info VARCHAR[1024] NOT NULL, epoch_time INTEGER NOT NULL, iso_time VARCHAR[64] NOT NULL, PRIMARY KEY _id);
+ CREATE TABLE auditing_dmp(_id VARCHAR[128] NOT NULL,user_id VARCHAR[128] NOT NULL,api VARCHAR[128] NOT NULL,method VARCHAR[32] NOT NULL,info VARCHAR[1024] NOT NULL, epoch_time INTEGER NOT NULL, iso_time VARCHAR[64] NOT NULL, PRIMARY KEY _id);
 
 
 ---
 --- auditing_consent
 ---
-exec create table auditing_consent (_id varchar[128] not null,item_id varchar not null,item_type varchar not null,event varchar not null,aiu_id varchar not null,aip_id varchar not null,dp_id varchar not null,isotime varchar not null,artifact varchar not null,shalog varchar not null,primary key _id);
-
+create table auditing_consent (_id varchar[128] not null,item_id varchar[256] not null,item_type varchar[128] not null,event varchar[256] not null,aiu_id varchar[128] not null,aip_id varchar[256] not null,dp_id varchar[128] not null,isotime varchar[128] not null,artifact varchar[128] not null,shalog varchar[2046] not null,primary key _id);
 ---
 --- auditing_acl_apd
--- This is not the current schema used in Dev instance of immudb, to create
--- this schema, data from the old table could be migrated here
 ---
-exec CREATE TABLE auditing_acl_apd(id VARCHAR[256] NOT NULL, userid VARCHAR[128] NOT NULL,endpoint VARCHAR[128] NOT NULL,method VARCHAR[128] NOT NULL,body VARCHAR[2048] NOT NULL,size INTEGER NOT NULL,isotime VARCHAR[128] NOT NULL,epochtime INTEGER NOT NULL,PRIMARY KEY id);
+
+ CREATE TABLE auditing_acl_apd( id VARCHAR[128] NOT NULL, userid VARCHAR[128] NOT NULL, endpoint VARCHAR[128] NOT NULL, method VARCHAR[128] NOT NULL, body VARCHAR[128] NOT NULL,size INTEGER NOT NULL,isotime VARCHAR[128] NOT NULL, epochtime INTEGER NOT NULL, PRIMARY KEY id);
 
 ---
 --- rsaudit
 ---
-exec CREATE TABLE rsaudit (id VARCHAR(128) PRIMARY KEY, api VARCHAR(128) NOT NULL, userid VARCHAR(128) NOT NULL, epochtime INTEGER NOT NULL, resourceid VARCHAR(256) NOT NULL, isotime VARCHAR(64) NOT NULL, providerid VARCHAR(128) NOT NULL, size INTEGER);
+ CREATE TABLE rsaudit (id VARCHAR[128] NOT NULL,api VARCHAR[128] NOT NULL,userid VARCHAR[128] NOT NULL,epochtime INTEGER NOT NULL,resourceid VARCHAR[256] NOT NULL,isotime VARCHAR[64] NOT NULL,providerid VARCHAR[128] NOT NULL,size INTEGER, PRIMARY KEY id);
 
 ---
 --- auditingtable
 ---
-exec CREATE TABLE auditingtable (id VARCHAR(128) PRIMARY KEY, userrole VARCHAR(64) NOT NULL, userid VARCHAR(128) NOT NULL, iid VARCHAR(250) NOT NULL, api VARCHAR(128) NOT NULL, method VARCHAR(32) NOT NULL, time INTEGER NOT NULL, iudxid VARCHAR(256) NOT NULL);
+ CREATE TABLE auditingtable (id VARCHAR[128] NOT NULL, userRole VARCHAR[64] NOT NULL,userID VARCHAR[128] NOT NULL,iid VARCHAR[250] NOT NULL,api VARCHAR[128] NOT NULL,method VARCHAR[32] NOT NULL,time INTEGER NOT NULL,iudxID VARCHAR[256] NOT NULL,PRIMARY KEY id);
 
+
+---
+--- ogcaudit
+---
+
+CREATE TABLE ogcaudit (id VARCHAR[128] NOT NULL, userid VARCHAR[128] NOT NULL, api VARCHAR[128] NOT NULL, resourceid VARCHAR[128] NOT NULL, providerid VARCHAR[128] NOT NULL, resource_group VARCHAR[128] NOT NULL, epochtime VARCHAR[256] NOT NULL, isotime VARCHAR NOT NULL, size VARCHAR NOT NULL, PRIMARY KEY (id));
 
 ---
 --- creating index for the tables
@@ -39,21 +43,30 @@ exec CREATE TABLE auditingtable (id VARCHAR(128) PRIMARY KEY, userrole VARCHAR(6
 ---
 --- auditing_dmp
 ---
-exec CREATE INDEX IF NOT EXISTS ON auditing_dmp(api, method, user_id, epoch_time);
+ CREATE INDEX IF NOT EXISTS ON auditing_dmp(api, method, user_id, epoch_time);
 
 ---
 --- auditing_acl_apd
 ---
-exec CREATE INDEX IF NOT EXISTS ON auditing_acl(endpoint, method, userid, epochtime);
+ CREATE INDEX IF NOT EXISTS ON auditing_acl_apd(userid,endpoint,epochtime);
 
 ---
 --- rsaudit
 ---
-CREATE INDEX IF NOT EXISTS index_name ON rsaudit(userid, providerid);
+CREATE INDEX IF NOT EXISTS rsaudit ON rsaudit(userid, epochtime, providerid);
 
 
 ---
 --- auditingtable
 ---
-CREATE INDEX IF NOT EXISTS index_name ON auditingtable(userid, api);
+CREATE INDEX IF NOT EXISTS auditingtable ON auditingtable(userID, iudxID, time);
 
+---
+--- auditing_consent
+---
+CREATE INDEX IF NOT EXISTS auditing_consent ON auditing_consent(item_id, aiu_id, dp_id);
+
+---
+--- ogcaudit
+---
+CREATE INDEX IF NOT EXISTS ogcaudit ON ogcaudit (userid, resourceid, providerid);
