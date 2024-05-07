e5a231102a1b538a93023406498a4cc386f67fda
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2024-05-07 14:06:57 +0530

    Add logs

--- src/main/java/iudx/auditing/server/querystrategy/DmpApdAuditingStrategy.java
+++ src/main/java/iudx/auditing/server/querystrategy/DmpApdAuditingStrategy.java
@@ -20,6 +20,8 @@ public class DmpApdAuditingStrategy implements AuditingServerStrategy {
 
   @Override
   public String buildPostgresWriteQuery(JsonObject request) {
+    LOGGER.debug("inside buildPostgresWriteQuery");
+    LOGGER.debug("request : {}", request.encodePrettily() );
     String primaryKey = request.getString(PRIMARY_KEY);
     String userId = request.getString(USER_ID);
     String api = request.getString(API);
@@ -45,6 +47,7 @@ public class DmpApdAuditingStrategy implements AuditingServerStrategy {
   @Override
   public String buildPostgresDeleteQuery(JsonObject request) {
     LOGGER.info("inside buildPostgresDeleteQuery");
+    LOGGER.debug("request : {}", request.encodePrettily() );
     String databaseTableName = config.getString(DMP_APD_PG_TABLE_NAME);
     String primaryKey = request.getString(PRIMARY_KEY);
     return DELETE_QUERY.replace("$0", databaseTableName).replace("$1", primaryKey);
@@ -52,6 +55,7 @@ public class DmpApdAuditingStrategy implements AuditingServerStrategy {
 
   @Override
   public String buildImmudbWriteQuery(JsonObject request) {
+    LOGGER.debug("inside buildImmudbWriteQuery");
     LOGGER.debug("request : {}", request.encodePrettily() );
     String primaryKey = request.getString(PRIMARY_KEY);
     String userId = request.getString(USER_ID);
