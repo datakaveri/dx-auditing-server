2d97cf9e09362e2f91bdfb4e77235e4029806c7b
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2024-05-08 18:22:46 +0530

    Add logs

--- src/main/java/iudx/auditing/server/processor/MessageProcessorImpl.java
+++ src/main/java/iudx/auditing/server/processor/MessageProcessorImpl.java
@@ -94,6 +94,7 @@ public class MessageProcessorImpl implements MessageProcessService {
   private Future<JsonObject> databaseOperations(JsonObject queries) {
     Promise<JsonObject> promise = Promise.promise();
     Future<JsonObject> insertInPostgres = postgresService.executeWriteQuery(queries);
+    LOGGER.debug("Queries from origin is {} , Query : {}", queries.getString(ORIGIN), queries.encode());
     insertInPostgres
         .onSuccess(
             insertInImmudbHandler -> {
