4c4e7fb2934883f043d4a0eea11ad806d23e5249
ananjaykumar2 <ananjaykumar2@gmail.com>
2024-06-25 11:49:44 +0530

    restore logger

--- src/main/java/iudx/auditing/server/processor/MessageProcessorImpl.java
+++ src/main/java/iudx/auditing/server/processor/MessageProcessorImpl.java
@@ -41,6 +41,7 @@ public class MessageProcessorImpl implements MessageProcessService {
   public Future<JsonObject> processAuditEventMessages(JsonObject message) {
     LOGGER.info("message processing starts : ");
     JsonObject queries = queryBuilder(message);
+    LOGGER.debug("message processing {}", queries);
     queries.put(DELIVERY_TAG, message.getLong(DELIVERY_TAG));
     queries.put(ORIGIN, message.getString(ORIGIN));
     Promise<JsonObject> promise = Promise.promise();
@@ -97,9 +98,10 @@ public class MessageProcessorImpl implements MessageProcessService {
   private Future<JsonObject> databaseOperations(JsonObject queries) {
     LOGGER.trace("databaseOperations started");
     Promise<JsonObject> promise = Promise.promise();
+    LOGGER.debug("Queries are : {}", queries.encode());
     Future<JsonObject> insertInPostgres = postgresService.executeWriteQuery(queries);
-   LOGGER.debug(
-        "Queries from origin: {} ", queries.getString(ORIGIN));
+    LOGGER.debug(
+        "Queries from origin is {} , Query : {}", queries.getString(ORIGIN), queries.encode());
     insertInPostgres
         .onSuccess(
             insertInImmudbHandler -> {
