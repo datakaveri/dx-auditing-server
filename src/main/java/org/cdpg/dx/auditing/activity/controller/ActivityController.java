0cc6ebe53b2f63e3077b9c532a9b9d90e7fad8ef
ananjaykumar2 <ananjaykumar2@gmail.com>
2025-05-05 19:40:55 +0530

    adde activity module's basik structure

---
+++ src/main/java/org/cdpg/dx/auditing/activity/controller/ActivityController.java
@@ -0,0 +1,9 @@
+package org.cdpg.dx.auditing.activity.controller;
+
+import io.vertx.ext.web.openapi.RouterBuilder;
+import org.cdpg.dx.auditing.apiserver.ApiController;
+
+public class ActivityController implements ApiController {
+  @Override
+  public void register(RouterBuilder builder) {}
+}
---
+++ src/main/java/org/cdpg/dx/auditing/activity/service/ActivityServiceImpl.java
@@ -0,0 +1,11 @@
+package org.cdpg.dx.auditing.activity.service;
+
+import io.vertx.core.Future;
+import org.cdpg.dx.auditing.activity.model.ActivityLog;
+
+public class ActivityServiceImpl implements ActivityService {
+  @Override
+  public Future<ActivityLog> getActivityLog(String userId) {
+    return null;
+  }
+}
