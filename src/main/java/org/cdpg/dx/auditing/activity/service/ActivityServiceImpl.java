83bba94ce14ab460287ed4f26cff6de290bad42d
ananjaykumar2 <ananjaykumar2@gmail.com>
2025-05-06 21:36:00 +0530

    fix compilation error

--- src/main/java/org/cdpg/dx/auditing/activity/service/ActivityServiceImpl.java
+++ src/main/java/org/cdpg/dx/auditing/activity/service/ActivityServiceImpl.java
@@ -1,11 +1,21 @@
 package org.cdpg.dx.auditing.activity.service;
 
 import io.vertx.core.Future;
+import java.util.List;
+import java.util.UUID;
+import org.cdpg.dx.auditing.activity.dao.ActivityLogDAO;
 import org.cdpg.dx.auditing.activity.model.ActivityLog;
 
 public class ActivityServiceImpl implements ActivityService {
+  private ActivityLogDAO activityLogDAO;
+
+  public ActivityServiceImpl(ActivityLogDAO activityLogDAO) {
+    this.activityLogDAO = activityLogDAO;
+  }
+
   @Override
-  public Future<ActivityLog> getActivityLog(String userId) {
-    return null;
+  public Future<List<ActivityLog>> getActivityLogByUserId(UUID userId) {
+   return activityLogDAO.getAllActivityLogsByUserId(userId);
+
   }
 }
