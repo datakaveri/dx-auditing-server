663cd743fbd2facb149d71a5c83e66fbae1d223f
shreelakshmijoshi <shreelakshmi.joshi@datakaveri.org>
2025-06-06 16:06:58 +0530

    Refactor: handle exception

--- src/main/java/org/cdpg/dx/common/exception/DxImmudbQueryException.java
+++ src/main/java/org/cdpg/dx/common/exception/DxImmudbQueryException.java
@@ -1,6 +1,6 @@
 package org.cdpg.dx.common.exception;
 
-public class DxImmudbQueryException extends DxPgException {
+public class DxImmudbQueryException extends BaseDxException {
     public DxImmudbQueryException(String message) {
         super(DxErrorCodes.IMMUDB_QUERY_ERROR, message);
     }
