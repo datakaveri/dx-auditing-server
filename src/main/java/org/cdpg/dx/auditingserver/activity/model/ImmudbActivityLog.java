7daa5f67e45be773b41f95670a1336b94a9e41db
Ankit Singh <101859999+ankitmashu@users.noreply.github.com>
2025-06-10 11:10:31 +0530

    bug fix

--- src/main/java/org/cdpg/dx/auditingserver/activity/model/ImmudbActivityLog.java
+++ src/main/java/org/cdpg/dx/auditingserver/activity/model/ImmudbActivityLog.java
@@ -1,18 +1,17 @@
 package org.cdpg.dx.auditingserver.activity.model;
 
 import io.vertx.core.json.JsonObject;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.cdpg.dx.auditingserver.activity.service.impl.ActivityServiceImpl;
-import org.cdpg.dx.database.postgres.util.EntityUtil;
+import org.cdpg.dx.auditingserver.activity.util.ActivityConstants;
 import org.cdpg.dx.database.immudb.util.DbConstants;
+import org.cdpg.dx.database.postgres.util.EntityUtil;
 
 /**
- * Represents an activity log entry in the Immudb database.
- * This record is used to store and retrieve activity logs related to assets and operations.
+ * Represents an activity log entry in the Immudb database. This record is used to store and
+ * retrieve activity logs related to assets and operations.
  */
 public record ImmudbActivityLog(
     String id,
@@ -28,8 +27,7 @@ public record ImmudbActivityLog(
     String userId,
     String originServer,
     String myactivityEnabled,
-    String shortDescription
-) {
+    String shortDescription) {
   private static final Logger LOGGER = LogManager.getLogger(ImmudbActivityLog.class);
 
   public static ImmudbActivityLog fromJson(JsonObject json) {
@@ -42,13 +40,12 @@ public record ImmudbActivityLog(
         json.getString(DbConstants.DB_CREATED_AT),
         json.getString(DbConstants.DB_API),
         json.getString(DbConstants.DB_METHOD),
-        json.getString(DbConstants.DB_SIZE),
+        json.getString(DbConstants.DB_SIZE, String.valueOf(0L)),
         json.getString(DbConstants.DB_ROLE),
         json.getString(DbConstants.DB_USER_ID),
         json.getString(DbConstants.DB_ORIGIN_SERVER),
         json.getString(DbConstants.DB_MYACTIVITY_ENABLED),
-        json.getString(DbConstants.DB_SHORT_DESCRIPTION)
-    );
+        json.getString(DbConstants.DB_SHORT_DESCRIPTION));
   }
 
   public Map<String, Object> toNonEmptyFieldsMap() {
@@ -69,5 +66,30 @@ public record ImmudbActivityLog(
     EntityUtil.putIfNonEmpty(map, DbConstants.DB_SHORT_DESCRIPTION, shortDescription);
     LOGGER.debug("Converted ImmudbActivityLog to non-empty fields map: {}", map);
     return map;
+  }
+
+  public JsonObject toJson() {
+    JsonObject json = new JsonObject();
+    putIfNonEmpty(json, ActivityConstants.ID, id);
+    putIfNonEmpty(json, ActivityConstants.ASSET_NAME, assetName);
+    putIfNonEmpty(json, ActivityConstants.ASSET_TYPE, assetType);
+    putIfNonEmpty(json, ActivityConstants.OPERATION, operation);
+    putIfNonEmpty(json, ActivityConstants.CREATED_AT, createdAt);
+    putIfNonEmpty(json, ActivityConstants.ASSET_ID, assetId);
+    putIfNonEmpty(json, ActivityConstants.API, api);
+    putIfNonEmpty(json, ActivityConstants.METHOD, method);
+    putIfNonEmpty(json, ActivityConstants.SIZE, size);
+    putIfNonEmpty(json, ActivityConstants.ROLE, role);
+    putIfNonEmpty(json, ActivityConstants.USER_ID, userId);
+    putIfNonEmpty(json, ActivityConstants.ORIGIN_SERVER, originServer);
+    putIfNonEmpty(json, ActivityConstants.SHORT_DESCRIPTION, shortDescription);
+    putIfNonEmpty(json, ActivityConstants.MYACTIVITY_ENABLED, myactivityEnabled);
+    return json;
+  }
+
+  private <T> void putIfNonEmpty(JsonObject json, String key, T value) {
+    if (value != null) {
+      json.put(key, value);
+    }
   }
 }
